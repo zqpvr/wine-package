c268b165b935abb3fdb150f80a8b491b997e8d6d
zqpvr <96351311+zqpvr@users.noreply.github.com>
2023-04-28 12:41:24 -0300

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -22,7 +22,8 @@ function install_box64() {
     dialog --infobox "Adding Box64 repository and installing packages..." $DIALOG_HEIGHT $DIALOG_WIDTH
     sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
     wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
-    sudo apt update && sudo apt install box64-tegrax1 -y
+    sudo apt update 
+    sudo apt install box64-tegrax1 -y
 }
 
 # Define function to install Box86 Binary
@@ -30,7 +31,8 @@ function install_box86() {
     dialog --infobox "Adding Box86 repository and installing packages..." $DIALOG_HEIGHT $DIALOG_WIDTH
     sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
     wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
-    sudo apt update && sudo apt install box86-tegrax1 -y
+    sudo apt update 
+    sudo apt install box86-tegrax1 -y
 }
 
 # Define function to install Wine and dependencies
@@ -64,7 +66,7 @@ function install_packages() {
 # Define function to compile and install Box64 from source
 function compile_install_box64() {
     dialog --infobox "Compiling Box64 from source..." $DIALOG_HEIGHT $DIALOG_WIDTH
-    sudo apt install zenity cmake git build-essential gcc-11 g++-11 -y || error "Could not install dependencies"
+    sudo apt install zenity cmake git build-essential gcc-11 g++-11 -y
     git clone --depth=1 https://github.com/ptitSeb/box64
     cd box64
     mkdir build
@@ -76,16 +78,17 @@ function compile_install_box64() {
 # Define function to compile and install Box86 from source
 function compile_install_box86() {
     dialog --infobox "Compiling Box86 from source..." $DIALOG_HEIGHT $DIALOG_WIDTH
-    sudo apt install gcc-8-arm-linux-gnueabihf -y || error "Could not install dependencies"
-    git clone https://github.com/ptitSeb/box86.git 
+    sudo apt install gcc-8-arm-linux-gnueabihf -y
+    git clone https://github.com/ptitSeb/box86.git
     cd box86 
+    git checkout 54e13be02a 
     mkdir build 
     cd build 
-    cmake .. -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc-8 -DARM_DYNAREC=ON || error "Compilation failed"
+    cmake .. -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc-8 -DARM_DYNAREC=ON
     echo "Building Box86"
     make -j$(nproc)
     echo "Still Building Box86"
-    sudo make install || error "Make install failed"
+    sudo make install
     sudo systemctl restart systemd-binfmt
 }
 
